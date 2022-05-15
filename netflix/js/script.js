c59fd3b8a062570c9a42a9b8bf18f29bf81ed4c4
Jamesordonez <jamesordonez308@gmail.com>
2022-05-15 16:29:13 -0500

    add change to netlix

--- netflix/js/script.js
+++ netflix/js/script.js
@@ -4,3 +4,9 @@ function mostrarmensaje(){
 function intercambiarImagen (ruta) {
     document.getElementById("imgPrincipal").src = ruta;
 }
+function textov(texto) {
+    document.getElementById("textoPrincipal").textContent = texto;
+}
+function bodyi(contenido){
+    document.getElementById("cuerpoP").textContent = contenido;
+}
